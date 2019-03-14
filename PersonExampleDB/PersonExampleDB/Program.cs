7cf0bffaee08b02769740337c7a5eab324d89c54
TeemuHe <teemuhoo@luukku.com>
2019-03-14 08:54:16 +0200

    delete function done

--- PersonExampleDB/PersonExampleDB/Program.cs
+++ PersonExampleDB/PersonExampleDB/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using PersonExampleDB.Models;
+using PersonExampleDB.Repositories;
 
 namespace PersonExampleDB
 {
     class Program
     {
+        private static readonly PersonRepository _personRepository = new PersonRepository();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Testing database Read
+            //CreatePerson();     used and added person once
+            ReadByCity();
+            for (int i = 0; i < 100; i++)
+            {
+                ReadById(i);
+            }
+        }
+
+        static void CreatePerson()
+        {
+            Person person = new Person();
+            person.FirstName = "Teemu";
+            person.LastName = "Heikkinen";
+            person.City = "Suonenjoki";
+            person.ShoeSize = 42;
+
+            _personRepository.Create(person);
+        }
+
+        static void ReadByCity()
+        {
+            var persons = _personRepository.ReadByCity("Suonenjoki");
+            
+            foreach (var p in persons)
+            {
+                Console.WriteLine($"{p.Id} {p.FirstName} {p.LastName} {p.City}");
+            }
+
+            Console.WriteLine("-----------------------------------");
+        }
+
+        static void ReadById(long id)
+        {
+            var person = _personRepository.ReadById(id);
+
+            if (person == null)
+                Console.WriteLine($"Asiakasta numerolla {id} ei löydy!");
+            else
+                Console.WriteLine($"{person.Id} {person.FirstName} {person.LastName} {person.City}");
         }
     }
 }
